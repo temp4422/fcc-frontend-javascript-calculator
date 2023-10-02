5f0761b2979fd3dbccc9d0ffe867ebd1cbeb43f5
user <user@MacBook-Pro-user.local>
2023-10-02 09:42:00 +0300

    Add handleClick 'CE' functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 'use client'
+import { ok } from 'assert'
+import { useState } from 'react'
 
 // Don't change order!
 const DATA = [
-  { id: 'PADDING', sign: '' },
-  { id: 'PADDING', sign: '' },
+  { id: 'PADDING1', sign: '' },
+  { id: 'PADDING2', sign: '' },
   { id: 'clear', sign: 'CE' },
   { id: 'divide', sign: '/' },
   { id: 'seven', sign: '7' },
@@ -18,24 +20,39 @@ const DATA = [
   { id: 'two', sign: '2' },
   { id: 'three', sign: '3' },
   { id: 'add', sign: '+' },
-  { id: 'PADDING', sign: '' },
+  { id: 'PADDING3', sign: '' },
   { id: 'zero', sign: '0' },
   { id: 'decimal', sign: '.' },
   { id: 'equals', sign: '=' },
 ]
 
 export default function Home() {
+  const [display, setDisplay] = useState(0)
+
   const buttons = DATA.map((item) => (
     <button
       key={item.id}
       id={item.id}
-      // Use different color for 'equals sign
+      // Use different color for 'equals' sign
       className={`rounded-md hover:brightness-125 active:bg-red-500
       ${item.id == 'equals' ? 'bg-[#4dc2fe]' : 'bg-[#3a3a3b]'}`}
+      onClick={handleClick}
     >
       {item.sign}
     </button>
   ))
+
+  function handleClick(e: any) {
+    const button = e.target.innerText
+
+    if (button === 'CE') {
+      setDisplay(0)
+    }
+
+    if ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9].includes(+button)) {
+      setDisplay(display + button)
+    }
+  }
 
   return (
     <div className="h-screen grid grid-rows-[auto_1fr_auto] text-center">
@@ -44,7 +61,7 @@ export default function Home() {
       <main className="flex flex-col items-center">
         <div className="min-w-[260px] max-w-[260px] h-96 bg-[#202121] rounded-md">
           <div id="display" className="w-full h-1/4 py-8 px-2 text-2xl text-right">
-            1234567890{' '}
+            {display}
           </div>
 
           <div id="buttons" className="w-full h-3/4 p-1 grid grid-cols-4 grid-rows-5 gap-1">
